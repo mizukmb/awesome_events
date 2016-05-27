2debab0ec05cd2de8debeb9f9dcb9ec1424fe153
mizukmb <mizukmb6@gmail.com>
2016-05-27 14:46:12 +0900

    spec/models/event_spec.rb のリファクタリング

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
   describe '#name' do
     context '空白のとき' do
+      let(:event) { Event.new(name: '') }
+
       it 'valid でないこと' do
-        event = Event.new(name: '')
         event.valid?
         expect(event.errors[:name]).to be_present
       end
