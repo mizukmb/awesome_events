e18a379699eca1ced6067f0e8727febc2afc52eb
mizukmb <mizukmb6@gmail.com>
2016-05-27 11:43:56 +0900

    機能テストはcapybaraの推奨する書き方に準拠する

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,19 +1,21 @@
 require 'rails_helper'
 
-describe 'ユーザがイベントの参加や登録を行うために、ログインをする' do
-  context 'トップページに遷移し、"Twitter でログイン"をクリックした時' do
-    context 'かつTwitterでのログインに成功した時' do
-      before do
-        visit root_path
-        click_link 'Twitter でログイン'
-      end
+feature 'ログイン' do
+  describe 'ユーザがイベントの参加や登録を行うために、ログインをする' do
+    context 'トップページに遷移し、"Twitter でログイン"をクリックした時' do
+      context 'かつTwitterでのログインに成功した時' do
+        before do
+          visit root_path
+          click_link 'Twitter でログイン'
+        end
 
-      it 'トップページに遷移していること' do
-        expect(page.current_path).to eq root_path
-      end
+        it 'トップページに遷移していること' do
+          expect(page.current_path).to eq root_path
+        end
 
-      it '":ログインしました"と表示されること' do
-        expect(page).to have_content 'ログインしました'
+        it '":ログインしました"と表示されること' do
+          expect(page).to have_content 'ログインしました'
+        end
       end
     end
   end
