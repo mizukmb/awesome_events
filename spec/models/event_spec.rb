e40f1cfd81b6c65604bb472cceb117a01e67ebaf
mizukmb <mizukmb6@gmail.com>
2016-05-30 10:37:05 +0900

    shoulda-matchers を使ったテストコードのリファクタリング

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   describe '#name' do
-    context '空白のとき' do
-      let(:event) { Event.new(name: '') }
-
-      it 'valid でないこと' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
+    it { should validate_presence_of(:name) }
   end
 end
