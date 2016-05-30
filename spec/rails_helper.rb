1c82069788be31ca517fd67ab085bf19d2f5be66
mizukmb <mizukmb6@gmail.com>
2016-05-30 12:35:53 +0900

    インデント修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,6 +41,6 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-	config.include(Shoulda::Matchers::ActiveModel, type: :model)
-	config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
