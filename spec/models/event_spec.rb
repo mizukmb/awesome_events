3030542305155192ccd454c4fff5c3447edfb73b
mizukmb <mizukmb6@gmail.com>
2016-05-30 11:02:14 +0900

    Event#content は空白の状態では validation エラーになる

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -9,5 +9,7 @@ RSpec.describe Event, type: :model do
     it { should validate_presence_of(:place) }
 
     it { should validate_length_of(:place).is_at_most(100) }
+
+    it { should validate_presence_of(:content) }
   end
 end
