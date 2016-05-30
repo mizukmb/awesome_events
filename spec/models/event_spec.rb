ac00217f99e1555e9c61b430b38b21d047e8493d
mizukmb <mizukmb6@gmail.com>
2016-05-30 11:00:24 +0900

    場所名は100文字以下

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe Event, type: :model do
     it { should validate_length_of(:name).is_at_most(50) }
 
     it { should validate_presence_of(:place) }
+
+    it { should validate_length_of(:place).is_at_most(100) }
   end
 end
