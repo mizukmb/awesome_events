6b9acbaebcb0fcbc8448a25e7983652d8e75760a
mizukmb <mizukmb6@gmail.com>
2016-05-30 11:04:00 +0900

    イベントの詳細は2000文字以下とする

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -11,5 +11,7 @@ RSpec.describe Event, type: :model do
     it { should validate_length_of(:place).is_at_most(100) }
 
     it { should validate_presence_of(:content) }
+
+    it { should validate_length_of(:content).is_at_most(2000) }
   end
 end
