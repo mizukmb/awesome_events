68047b1ce87565f85d730b7246553503c237ae4e
mizukmb <mizukmb6@gmail.com>
2016-05-30 11:07:33 +0900

    Event#start_time は空白の状態では validation エラーになる

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -13,5 +13,7 @@ RSpec.describe Event, type: :model do
     it { should validate_presence_of(:content) }
 
     it { should validate_length_of(:content).is_at_most(2000) }
+
+    it { should validate_presence_of(:start_time) }
   end
 end
