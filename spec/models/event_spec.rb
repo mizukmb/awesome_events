0f684c0981c3a3d1f6454ec2578bfb17d7dcd66f
mizukmb <mizukmb6@gmail.com>
2016-05-30 10:58:37 +0900

    Event#place は空白の状態では validation エラーになる

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Event, type: :model do
     it { should validate_presence_of(:name) }
 
     it { should validate_length_of(:name).is_at_most(50) }
+
+    it { should validate_presence_of(:place) }
   end
 end
