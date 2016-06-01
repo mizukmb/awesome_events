087e19ad81d1cbe2f6f5ae9415a31c38c2e2e63d
mizukmb <mizukmb6@gmail.com>
2016-06-01 11:55:57 +0900

    FactoryGirl の User は :place ではなく :nickname カラムを作成する

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user, aliases: [:owner] do
     provider 'twitter'
     sequence(:uid) { |i| "uid#{i}" }
-    sequence(:place) { |i| "nickname#{i}" }
+    sequence(:nickname) { |i| "nickname#{i}" }
     sequence(:image_url) { |i| "http://example.com/image/#{i}.jpg" }
   end
 end
