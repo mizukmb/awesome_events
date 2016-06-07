00dc5e43d22c75a171efce6f2310e21c87f54922
mizukmb <mizukmb6@gmail.com>
2016-06-07 16:32:44 +0900

    ステージング環境のデプロイ設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:mizukmb/awesome_events.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/var/www/awesome_events'
+set :deploy_to, '/var/www/awesome-events'
 
 # Default value for :scm is :git
 set :scm, :git
